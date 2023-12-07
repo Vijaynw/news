18f8dec90a38182da940a88712629a558eafc382
Vijay Narsinh Waghmare <vijaywaghmare713@gmail.com>
2023-12-07 23:37:57 +0530

    Search featur

---
+++ src/component/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+      <h1>
+    We are here to told the truth !🎯    
+      </h1>
+    
+    </div>
+  )
+}
+
+export default About
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -4,25 +4,31 @@ const NewsItem = ({ title, description, imageUrl, newsUrl }) => {
   console.log(newsUrl)
   return (
   
-    <div className="card" style={{ width: "18rem" }}>
-      <img
+    <div className=' m-2 w-2/12'>
+
+    <div className=" shadow-2xl w-12/12 h-[550px]" >
+      <img className='p-2 m-2'
         src={
           !imageUrl
-            ? "https://cdndh.darkhorizons.com/wp-content/uploads/2023/02/amazon-content-spending-hits-16-billion.jpg"
-            : imageUrl
+          ? "https://cdndh.darkhorizons.com/wp-content/uploads/2023/02/amazon-content-spending-hits-16-billion.jpg"
+          : imageUrl
         }
-        className="card-img-top"
+        
         alt="..."
-      />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        />
+      <div className="p-2">
+        <h5 className="">{title}</h5>
+        <p className="">{description}</p>
+      </div>
+      <div className='bg-black absolute '>
+
         <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">
           Read More
         </a>
       </div>
     </div>
 
+        </div>
   
   )
 }
